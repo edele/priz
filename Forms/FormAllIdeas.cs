9deeb4eec48392a8451de9e7fc7053269fa0af54
Jerico174 <jericono@gmail.com>
2014-03-12 00:21:39 +0600

    Сортировка идей

--- Forms/FormAllIdeas.cs
+++ Forms/FormAllIdeas.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.Diagnostics;
+using System.Collections;
 
 namespace PRIZ
 {
@@ -27,11 +28,7 @@ namespace PRIZ
             btnLogoEducationEra.MouseEnter += Program.LogoMouseEnter;
             btnLogoEducationEra.MouseLeave += Program.LogoMouseLeave;
 
-            foreach (string hypotesis in answer._hypothesises)
-            {
-                tbAllHypo.Text += "• " + hypotesis;
-                tbAllHypo.Text += "\r\n";
-            }
+            
         }
 
         private void btnPrint_Click(object sender, EventArgs e)
@@ -144,6 +141,101 @@ namespace PRIZ
                 Program.fLogin.Show();
                 this.Hide();
             }
+        }
+
+        private void FormAllIdeas_Load(object sender, EventArgs e)
+        {
+
+            for (int i = 0; i < answer._hypothesises.Count; i++)
+            {
+                lAllHypo.Items.Add(answer._hypothesises[i]);
+            }
+        }
+    }
+    //______________________________________________________________________________
+
+    public class UserSortableListBox : ListBox
+    {
+        /// <summary>
+        /// Reordered elements indices
+        /// </summary>
+        public class ReorderEventArgs : EventArgs
+        {
+            public int index1, index2;
+        }
+        /// <summary>
+        /// Reorder event handler
+        /// </summary>
+        public delegate void ReorderHandler(object sender, ReorderEventArgs e);
+        public event ReorderHandler Reorder;
+
+        //AllowDrop must be set to true in order to make drag'n'drop working
+        [Browsable(false)]
+        new public bool AllowDrop
+        {
+            get { return true; }
+            set { }
+
+        }
+        public UserSortableListBox()
+        {
+            base.AllowDrop = true;
+            base.SelectionMode = SelectionMode.One;
+        }
+
+        [Browsable(false)]
+        new public SelectionMode SelectionMode
+        {
+            get { return SelectionMode.One; }
+            set { }
+        }
+        /// <summary>
+        /// Index of dragged element
+        /// </summary>
+        private int sourceIndex = 0;
+        protected override void OnDragDrop(DragEventArgs e)
+        {
+            base.OnDragDrop(e);
+            Point point = PointToClient(new Point(e.X, e.Y));
+            int index = IndexFromPoint(point); //destination index
+            //allow DataSource, i.e. BindingList
+            IList items = DataSource != null ? DataSource as IList : Items;
+            if (index < 0) index = items.Count - 1;
+            if (index != sourceIndex)
+            {
+                if (index > sourceIndex) //destination is below source
+                {
+                    items.Insert(index + 1, items[sourceIndex]);
+                    items.RemoveAt(sourceIndex);
+                }
+                else //destination is above source
+                {
+                    items.Insert(index, items[sourceIndex]);
+                    items.RemoveAt(sourceIndex + 1);
+                }
+                if (null != Reorder) Reorder(this, new ReorderEventArgs() { index1 = sourceIndex, index2 = index });
+            }
+            //selectedIndex was lost during reorder
+            SelectedIndex = index;
+        }
+
+        protected override void OnDragOver(DragEventArgs e)
+        {
+            base.OnDragOver(e);
+            e.Effect = DragDropEffects.Move | DragDropEffects.Scroll;
+        }
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+            if (SelectedItem == null)
+            {
+                return;
+            }
+            sourceIndex = SelectedIndex;
+            //OnSelectedIndexChanged is not launched while using MouseDown :(
+            OnSelectedIndexChanged(e);
+            DoDragDrop(SelectedItem, DragDropEffects.Move);
         }
     }
 }
